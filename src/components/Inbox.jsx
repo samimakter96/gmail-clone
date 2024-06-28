ba291db1bd4c63f355bd8b4cd5dd13aeaf3a974b
samim akter <samimakter9641@gmail.com>
2024-06-28 23:25:54 +0530

    inside inbox message added

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import { FaCaretDown, FaUserFriends } from "react-icons/fa";
 import { GoTag } from "react-icons/go";
 import { IoMdMore, IoMdRefresh } from "react-icons/io";
-import { MdCropSquare, MdInbox } from "react-icons/md";
+import {
+  MdCropSquare,
+  MdInbox,
+  MdKeyboardArrowLeft,
+  MdKeyboardArrowRight,
+} from "react-icons/md";
+import Messages from "./Messages";
 
 const mailType = [
   {
@@ -37,6 +43,15 @@ const Inbox = () => {
             <IoMdMore size={"20px"} />
           </div>
         </div>
+        <div className="flex items-center gap-2">
+          <p className="text-sm text-gray-500">1-50 of 1000</p>
+          <button className="hover:rounded-full hover:bg-gray-100">
+            <MdKeyboardArrowLeft size={"24px"} />
+          </button>
+          <button className="hover:rounded-full hover:bg-gray-100">
+            <MdKeyboardArrowRight size={"24px"} />
+          </button>
+        </div>
       </div>
       <div className="h-[90vh] overflow-y-auto">
         <div className="flex items-center gap-1">
@@ -57,6 +72,7 @@ const Inbox = () => {
             );
           })}
         </div>
+        <Messages />
       </div>
     </div>
   );
