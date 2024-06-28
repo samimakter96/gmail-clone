a8977305e872a896b122f29f92ef57a3f957d760
samim akter <samimakter9641@gmail.com>
2024-06-28 22:15:52 +0530

    create navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/shared/Navbar";
+
+const App = () => {
+  return (
+    <div className="bg-[#f6f8fc] h-screen w-screen overflow-hidden">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { GoQuestion } from "react-icons/go";
+import { IoSettingsOutline } from "react-icons/io5";
+import { PiDotsNineBold } from "react-icons/pi";
+import Avatar from "react-avatar";
+import samim_pic from '../../assets/samim-profile-pic.jpeg'
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between mx-3 h-16">
+      <div className="flex items-center gap-10">
+        <div className="flex items-center gap-2">
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <RxHamburgerMenu size={"20px"} />
+          </div>
+          <img
+            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
+            alt="gmail-logo"
+          />
+        </div>
+      </div>
+      <div className="md:block hidden w-[50%] mr-60">
+        <div className="flex items-center bg-[#eaf1fb] px-2 py-3 rounded-full">
+          <IoIosSearch size={"24px"} className="text-gray-700" />
+          <input
+            type="text"
+            placeholder="Search Mail"
+            className="rounded-full w-full bg-transparent outline-none px-1"
+          />
+        </div>
+      </div>
+      <div className="md:block hidden">
+        <div className="flex items-center gap-2">
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <GoQuestion size={"20px"} />
+          </div>
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <IoSettingsOutline size={"20px"} />
+          </div>
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <PiDotsNineBold size={"20px"} />
+          </div>
+          <div className="cursor-pointer">
+            <Avatar src={samim_pic} size="35" round={true}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
