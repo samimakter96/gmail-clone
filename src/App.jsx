192fb04afaf560eb80ced5f4f87316bc6a9c5fb4
samim akter <samimakter9641@gmail.com>
2024-06-28 22:17:45 +0530

    sidebar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/shared/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <div className="bg-[#f6f8fc] h-screen w-screen overflow-hidden">
       <Navbar />
+      <Sidebar />
     </div>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { MdInbox } from "react-icons/md";
+import { IoMdStar } from "react-icons/io";
+import { LuPencil } from "react-icons/lu";
+import { MdOutlineDrafts, MdOutlineKeyboardArrowDown, MdOutlineWatchLater } from "react-icons/md";
+import { TbSend2 } from "react-icons/tb";
+
+const sidebarItems = [
+  {
+    icon: <MdInbox size={"24px"} />,
+    text: "Inbox",
+  },
+  {
+    icon: <IoMdStar size={"24px"} />,
+    text: "Starred",
+  },
+  {
+    icon: <MdOutlineWatchLater size={"24px"} />,
+    text: "Snoozed",
+  },
+  {
+    icon: <TbSend2 size={"24px"} />,
+    text: "Sent",
+  },
+  {
+    icon: <MdOutlineDrafts size={"24px"} />,
+    text: "Draft",
+  },
+  {
+    icon: <MdOutlineKeyboardArrowDown size={"24px"} />,
+    text: "More",
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className="w-[15%]">
+      <div className="p-3">
+        <button className="flex items-center gap-2 p-4 rounded-2xl hover:shadow-md bg-[#c2e7ff]">
+          <LuPencil size={"24px"} />
+          Compose
+        </button>
+      </div>
+      <div className="text-gray-500">
+        {sidebarItems.map((item, index) => {
+          return (
+            <div className="flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer my-2 hover:bg-gray-200">
+              {item.icon}
+              <p>{item.text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
